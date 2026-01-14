904f48f2b3ca42899537dfc94ba8291d56435212
M Tahir Shabbir <tahirshabbir955@gmail.com>
2026-01-14 16:45:55 +0500

    made updates in frontend

--- config/apiConfig.ts
+++ config/apiConfig.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.eventcoresolutions.com";
 
